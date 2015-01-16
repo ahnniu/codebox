6fca220016b340fba0eabe44a6be03147ba6eaf8
Yongjia Niu <ahnniu@gmail.com>
2015-01-17 00:51:08 +0800

    added 'Font-Awesome' support

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,7 @@ module.exports = function (grunt) {
     // Load grunt modules
     grunt.loadNpmTasks('grunt-hr-builder');
     grunt.loadNpmTasks("grunt-bower-install-simple");
+    grunt.loadNpmTasks('grunt-font-awesome-vars');
     grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -35,6 +36,12 @@ module.exports = function (grunt) {
                 color:       true,
                 production:  false,
                 directory:   "editor/vendors"
+            }
+        },
+        "fontAwesomeVars": {
+            main: {
+                variablesLessPath: 'editor/vendors/fontawesome/less/variables.less',
+                fontPath: 'fonts/font-awesome'        //NOTE: this must be relative to FINAL, compiled .css file - NOT the variables.less file! For example, this would be the correct path if the compiled css file is main.css which is in 'src/build' and the font awesome font is in 'src/bower_components/font-awesome/fonts' - since to get from main.css to the fonts directory, you first go back a directory then go into bower_components > font-awesome > fonts.
             }
         },
         "hr": {
@@ -61,7 +68,8 @@ module.exports = function (grunt) {
                 "static": {
                     "fonts": path.resolve(srcPath, "resources/fonts"),
                     "images": path.resolve(srcPath, "resources/images"),
-                    "fonts/octicons": path.resolve(srcPath, "vendors/octicons/octicons")
+                    "fonts/octicons": path.resolve(srcPath, "vendors/octicons/octicons"),
+                    "fonts/font-awesome": path.resolve(srcPath, "vendors/fontawesome/fonts")
                 },
 
                 // Vendors
@@ -205,7 +213,8 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask("prepare", 'Prepare client build', [
-        'bower-install-simple'
+        'bower-install-simple',
+        'fontAwesomeVars'
     ]);
 
     grunt.registerTask('build', 'Build client code', [
